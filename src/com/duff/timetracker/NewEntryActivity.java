0d8caae41500078897be352297396241c7b9c40a
Mike Voytovich <mikev@duffresearch.com>
2012-03-28 23:22:33 -0700

    example arrayadapter

--- src/com/duff/timetracker/NewEntryActivity.java
+++ src/com/duff/timetracker/NewEntryActivity.java
@@ -2,6 +2,11 @@ package com.duff.timetracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Allows user to enter a new time tracking entry
@@ -14,5 +19,13 @@ public class NewEntryActivity extends Activity
     {
         super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_entry);
+		Spinner projectSpinner = (Spinner)findViewById(R.id.selectProject);
+		List<String> list = new ArrayList<String>();
+		list.add("list 2");
+		list.add("list 3");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		projectSpinner.setAdapter(dataAdapter);
     }
 }
