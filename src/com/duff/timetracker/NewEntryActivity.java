47a21250ed360285dcdf035ffae6e221a13c9ae8
Mike Voytovich <mikev@duffresearch.com>
2012-03-30 10:25:05 -0700

    submit in background, add progressdialog

--- src/com/duff/timetracker/NewEntryActivity.java
+++ src/com/duff/timetracker/NewEntryActivity.java
@@ -1,6 +1,9 @@
 package com.duff.timetracker;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -21,6 +24,7 @@ public class NewEntryActivity extends Activity
 	private Spinner mTaskSpinner;
 	private EditText mHoursEditText;
 	private Button mSubmitButton;
+	private Context mContext;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -33,6 +37,7 @@ public class NewEntryActivity extends Activity
 		mTaskSpinner = (Spinner)findViewById(R.id.selectTask);
 		mHoursEditText = (EditText)findViewById(R.id.enterHours);
 		mSubmitButton = (Button)findViewById(R.id.submit);
+		mContext = this;
 
 		initProjectSpinner();
 		initTaskSpinner();
@@ -98,24 +103,51 @@ public class NewEntryActivity extends Activity
 					return;
 				}
 
-				String project = (String)mProjectSpinner.getSelectedItem();
-				String task = (String)mTaskSpinner.getSelectedItem();
-				String hours = mHoursEditText.getText().toString();
-
-				SimpleDB.createDomain(SimpleDB.DOMAIN_NAME);
-
-				String newItem = java.util.UUID.randomUUID().toString();
-				SimpleDB.createItem(SimpleDB.DOMAIN_NAME, newItem);
-
-				SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.USER_ATTRIBUTE_NAME, AppPreferences.getUserName());
-
-				String date = new java.text.SimpleDateFormat("MM/dd/yyyy hh:mm:ss a").format(new java.util.Date());
-				SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.DATE_ATTRIBUTE_NAME, date);
-
-				SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.PROJECT_ATTRIBUTE_NAME, project);
-				SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.TASK_ATTRIBUTE_NAME, task);
-				SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.HOURS_ATTRIBUTE_NAME, hours);
+				SubmitEntryTask task = new SubmitEntryTask();
+				task.execute(null);
 			}
 		});
 	}
+
+	private class SubmitEntryTask extends AsyncTask<String, Void, String> {
+
+		private ProgressDialog mProgressDialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mProgressDialog = new ProgressDialog(mContext);
+			mProgressDialog.setMessage("Sending entry...");
+			mProgressDialog.show();
+
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			String project = (String)mProjectSpinner.getSelectedItem();
+			String task = (String)mTaskSpinner.getSelectedItem();
+			String hours = mHoursEditText.getText().toString();
+
+			SimpleDB.createDomain(SimpleDB.DOMAIN_NAME);
+
+			String newItem = java.util.UUID.randomUUID().toString();
+			SimpleDB.createItem(SimpleDB.DOMAIN_NAME, newItem);
+
+			SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.USER_ATTRIBUTE_NAME, AppPreferences.getUserName());
+
+			String date = new java.text.SimpleDateFormat("MM/dd/yyyy hh:mm:ss a").format(new java.util.Date());
+			SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.DATE_ATTRIBUTE_NAME, date);
+
+			SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.PROJECT_ATTRIBUTE_NAME, project);
+			SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.TASK_ATTRIBUTE_NAME, task);
+			SimpleDB.createAttributeForItem(SimpleDB.DOMAIN_NAME, newItem, SimpleDB.HOURS_ATTRIBUTE_NAME, hours);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			mProgressDialog.hide();
+		}
+	}
+
 }
